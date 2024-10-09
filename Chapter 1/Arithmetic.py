9eeb114a5fc2878b5e6f3ef05ec41cc462da8b64
Torchkill <torkiljt@gmail.com>
2024-10-09 14:21:14 +0100

    added arithmetic.py

---
+++ Chapter 1/Arithmetic.py
@@ -0,0 +1,44 @@
+"""
+Docstrings are used to explains what file or function is doing or
+if you're studying, it's ideal to show which assignment your solving 
+and sign with your name and date.
+
+In this code we simply look at the standard arithmetic operators
+"""
+
+# We'll assign variables to make it easier to graps and 
+# assign it to the operator variable.
+num_one = 10
+num_two = 2
+
+# Addition
+add = num_one + num_two
+
+# Subtraction
+sub = num_one - num_two
+
+# Multiplication
+multi = num_one * num_two
+
+# Exponentiation
+expo = num_one ** num_two
+
+# True division
+div = num_one / num_two
+
+# Floor division
+floor = num_one // num_two
+
+# Remainder Modulo
+
+mod = num_one % num_two
+
+# To ensure we understand the operators we can print them out
+
+print(add)
+print(sub)
+print(multi)
+print(expo)
+print(div)
+print(floor)
+print(mod)
--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Beginner_python
-A repository to help people get into coding
+A repository to help people get into coding.
 
 # To follow the guides
 Install Python either via https://www.python.org/downloads/ or through vscode extension.
 
 I will use Vscode IDE, but any IDE is fine to follow these simple examples that will be shown.
+
+# Book used as reference
+
+Intro to Python for Computer Science and Data Science
+Learning to Program with AI, Big Data and the Cloud.
+Deitel
